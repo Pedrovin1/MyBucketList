5e1c88b502df7cac4a0d0ec68f0077836017138a
Pedrovin1 <pedro.zancan18@gmail.com>
2024-09-22 14:57:46 -0300

    fixed List Index related bugs

--- Scenes/Telas/MainScreen/BucketEntriesManager.cs
+++ Scenes/Telas/MainScreen/BucketEntriesManager.cs
@@ -11,7 +11,7 @@ public partial class BucketEntriesManager : Node
     BucketListEntry selectedBucketItem = null;
     int maxEntriesPerScreen = 6;
 
-    List<string> dummyData = new List<string>();//{"1","2","13951"};//,"17999","14545","133","1444","17777","1333","1444","5451","11111","1123"
+    List<string> dummyData = new List<string>{"1","2","13951","1233333","mais cosai", "e mais", "e ainda mais","namoral","palceholer"};//,"17999","14545","133","1444","17777","1333","1444","5451","11111","1123"
 
     public override void _Ready()
     {
@@ -84,7 +84,7 @@ public partial class BucketEntriesManager : Node
         }
         if(vboxIndexSelectedItem > 0)
         {
-             selectedBucketItem = vboxComponentsList.GetChild<BucketListEntry>(vboxIndexSelectedItem);
+            selectedBucketItem = vboxComponentsList.GetChild<BucketListEntry>(vboxIndexSelectedItem);
             selectedBucketItem.selecionarItem();
         }
         
@@ -95,21 +95,27 @@ public partial class BucketEntriesManager : Node
         const int OFFSET = 1;
         List<string> newItemsRange;
 
-        BucketListEntry currentBucketItem; //clears the text of all eentries, to then update them
+        BucketListEntry currentBucketItem; //clears the text of all entries, to then update them
         for(int i = 0; i < maxEntriesPerScreen; i++)
         {
             currentBucketItem = vboxComponentsList.GetChild<BucketListEntry>(i + OFFSET);
             currentBucketItem.alterarTexto(string.Empty);
         }
 
-        if(listIndexInicial == -1)
+        if(listIndexInicial <= -1)
         {
+            if(selectedBucketItem != null )
+            {
+                selectedBucketItem.deselecionarItem();
+                selectedBucketItem = null;
+                vboxIndexSelectedItem = 0;
+            }
             return;
         }
 
-        if(dummyData.Count < maxEntriesPerScreen)
+        if(listIndexInicial + maxEntriesPerScreen > dummyData.Count)
         {
-            newItemsRange = dummyData.GetRange(listIndexInicial, dummyData.Count);
+            newItemsRange = dummyData.GetRange(listIndexInicial, dummyData.Count - listIndexInicial);
         }
         else
         {
@@ -148,13 +154,13 @@ public partial class BucketEntriesManager : Node
         if(vboxIndexSelectedItem > 0 && listIndexSelectedItem < dummyData.Count)
         {
             dummyData.RemoveAt(listIndexSelectedItem);
-            if(dummyData.Count <= 0)
+            if(this.listIndexFirstVisibleBucketItem >= dummyData.Count)
             {
-                this.listIndexFirstVisibleBucketItem = -1;
+                this.listIndexFirstVisibleBucketItem = dummyData.Count - 1;
             }
         }
 
-        this.updateVisibleBucketEntries(listIndexFirstVisibleBucketItem);
+        this.updateVisibleBucketEntries(this.listIndexFirstVisibleBucketItem);
     }
 
 
