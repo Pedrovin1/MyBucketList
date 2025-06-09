51589471d6f1f389f11851c2a561a12bc4fdd2e9
Pedrovin1 <pedro.zancan18@gmail.com>
2025-06-08 23:28:19 -0300

    implementação de métodos utilitários para manipular características do componente
     alterar cor da borda, fundo e texto do componente, alterar texto do componente e alterar grossura da borda do componente

--- Scenes/TitleTextBoxComponent/TitleTextBox.cs
+++ Scenes/TitleTextBoxComponent/TitleTextBox.cs
@@ -7,11 +7,15 @@ namespace CustomComponents;
 public partial class TitleTextBox : Control
 {
     private Vector2 defaultSize = new Vector2(100f, 50f);
+    private const int DefaultMargin = 4;
+    
 
     private Polygon2D _border;
     private Polygon2D _background;
     private RichTextLabel _text;
     private MarginContainer _marginContainer;
+
+    private int _borderThickness = 0;
 
     public override void _Ready()
     {
@@ -30,6 +34,32 @@ public partial class TitleTextBox : Control
         }
     }
 
+    public void changeBackgroundColor(Color color) => this._background.Color = color;
+    public void resetBackgroundColor() => this._background.Color = Colors.Black;
+
+    public void changeBorderColor(Color color) => this._border.Color = color;
+    public void resetBorderColor() => this._border.Color = Colors.Black;
+    public void changeBorderThickness(int pixelsAmount)
+    {
+        this._borderThickness = pixelsAmount;
+
+        //to shrink the background polygon2D to make the border polygon2D more visible
+        Vector2[] currentBgSize = this._background.Polygon;
+        this._background.Polygon = new Vector2[]
+        {
+            new Vector2(currentBgSize[0].X + pixelsAmount, currentBgSize[0].Y + pixelsAmount),
+            new Vector2(currentBgSize[1].X - pixelsAmount, currentBgSize[1].Y + pixelsAmount),
+            new Vector2(currentBgSize[2].X - pixelsAmount, currentBgSize[2].Y - pixelsAmount),
+            new Vector2(currentBgSize[3].X + pixelsAmount, currentBgSize[3].Y - pixelsAmount),
+        };
+
+        this._marginContainer.AddThemeConstantOverride("margin_left", DefaultMargin + pixelsAmount);
+        this._marginContainer.AddThemeConstantOverride("margin_right", DefaultMargin + pixelsAmount);
+    }
+
+    public void updateText(string newText) => this._text.Text = newText;
+    public void changeTextColor(Color color) => this._text.AddThemeColorOverride("default_color", color);
+    public void resetTextColor(Color color) => this._text.RemoveThemeColorOverride("default_color");
 
     public void onResized()
     {
@@ -56,5 +86,7 @@ public partial class TitleTextBox : Control
             new Vector2(x, y),
             new Vector2(0f, y)
         };
+
+        this.changeBorderThickness(this._borderThickness);
     }
 }
