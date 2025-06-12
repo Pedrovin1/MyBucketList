70471cdeab974912fad113c612eaa2c613d4a9bb
Pedrovin1 <pedro.zancan18@gmail.com>
2025-06-12 16:13:59 -0300

    implementação de função de exportar a bucketList para json no diretório AppData Local

--- Scripts/Singletons/UserData.cs
+++ Scripts/Singletons/UserData.cs
@@ -1,6 +1,62 @@
 using Godot;
 using System;
+using System.Collections.Generic;
+using System.IO;
 
 public partial class UserData : Node
 {
+    private List<BucketItem> _bucketItems;
+
+    private const string UserDataFolderName = "MyBucketList";
+    private const string UserDataFileName = "MyBucketListData.json";
+    private string UserDataFolderPath;
+
+    public static UserData Instance;
+
+    public override void _Ready()
+    {
+        UserData.Instance = this;
+
+        this.UserDataFolderPath =
+            System.Environment.GetFolderPath(System.Environment.SpecialFolder.LocalApplicationData)
+            + @"\" + UserData.UserDataFolderName;
+    }
+
+    public async void ExportData()
+    {
+        string jsonString = System.Text.Json.JsonSerializer.Serialize(this._bucketItems);
+        string saveFilePath = this.UserDataFolderPath + @"\" + UserData.UserDataFileName;
+
+        int counter = 0;
+        bool retry = true;
+        const int maxRetries = 3;
+
+        while (counter < maxRetries && retry)
+        {
+            counter++;
+            retry = false;
+
+            try
+            {
+                if (!Godot.DirAccess.DirExistsAbsolute(this.UserDataFolderPath))
+                {
+                    System.IO.Directory.CreateDirectory(this.UserDataFolderPath);
+                }
+                if (!Godot.FileAccess.FileExists(saveFilePath))
+                {
+                    System.IO.File.Create(saveFilePath).Close();
+                }
+
+                using (var file = Godot.FileAccess.Open(saveFilePath, Godot.FileAccess.ModeFlags.Write))
+                {
+                    file.StoreString(jsonString);
+                }
+            }
+            catch (IOException)
+            {
+                retry = true;
+                await ToSignal(GetTree().CreateTimer(0.3d), Timer.SignalName.Timeout);
+            }
+        }
+    }
 }
